bfe0dda6a060ec50730811cad2ee291f270d74b0
Sami Cemek <ahmet.cemek24@ncf.edu>
2021-11-29 21:56:48 -0500

    Created the become a volunteer page.

--- js/ValidateEmail.js
+++ js/ValidateEmail.js
@@ -1,14 +1,15 @@
 function ValidateEmail(inputText){
-  
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-if(inputText.value.match(mailformat)){
-  alert("Valid email address!");
-  document.form1.text1.focus();
-  return true;
-  } else {
-  alert("You have entered an invalid email address!");
-  document.form1.text1.focus();
-  return false;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var el_email = document.getElementByClass('email');   
+
+  if(inputText.value.match(mailformat)){
+    alert("Valid email address!");
+    document.el_email.focus();
+    return true;
+    } else {
+    alert("You have entered an invalid email address!");
+    document.el_email.focus();
+    return false;
   }
 }
