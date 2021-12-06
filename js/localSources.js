ac9817f3eb3fffc1d996f152a29dc6f53a35b0d7
Sami Cemek <ahmet.cemek24@ncf.edu>
2021-12-05 21:02:32 -0500

    Finished the Sources page.

---
+++ js/localSources.js
@@ -0,0 +1,5 @@
+function localSources(value)
+{
+	document.getElementById("demo").innerHTML = ("Showing local sources for: " + value);
+	document.getElementById("demo1").innerHTML = ("You can attend volunteering activities in " + value + ".");
+}
